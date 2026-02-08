175133c0b75ae29e90c837be3df1fd56048ce638
Oussama12520 <oussemamasmoudi7@gmail.com>
2026-02-08 22:03:08 +0100

    fix: remove incorrect index.css import

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './index.css';
 import { getProjects, forceRefreshFromGitHub } from './services/storageService';
 import { Project, ProjectCategory } from './types';
 import Admin from './components/Admin';
